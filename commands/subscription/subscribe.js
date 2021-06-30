6d253c4c00f5132994a14ba0c409caa7c6c14bd2
kcai8672 <kcai8672@uni.sydney.edu.au>
2021-07-01 01:50:11 +1000

    Added comments to subscription commands

--- commands/subscription/subscribe.js
+++ commands/subscription/subscribe.js
@@ -7,15 +7,18 @@ module.exports = {
     usage: '<channelNames | all>',
     guildOnly: true,
     execute(message, args) {
+        // load in stored data
         const JSONData = fs.readFileSync('data/database.json')
         const newData = JSON.parse(JSONData)
         const guild = message.guild
         const member = message.member
+        // find matching server in data
         const server = newData.find((element) => element.serverID === guild.id)
+        // extract all voice channels in server
         const vcAll = guild.channels.cache.filter((channel) => channel.type === 'voice')
         let flag = true
 
-        // attempt to add to all voice channels
+        // 'all' arg specified, attempting to subscribe to all voice channels
         if (args[0] === 'all' && args.length === 1) {
             server.vc.forEach((channel) => {
                 const exists = channel.subscribed.some((uid) => uid === member.id)
@@ -23,29 +26,34 @@ module.exports = {
                     channel.subscribed.push(member.id)
                 }
             })
-        } else {
-            // only add to specified channels in args
+        } 
+        // attempt to subscribe to all specified channel names
+        else {
             for (let i = 0; i < args.length; i++) {
+                // finding voice channel matching given name
                 const vc = vcAll.find((channel) => channel.name.toLowerCase() === args[i])
                 if (!vc) {
                     flag = false
                     break
                 }
+                // finding matching voice channel data entry
                 const found = server.vc.find((channel) => channel.vcID === vc.id)
                 if (!found) {
                     flag = false
                     break
                 }
+                // member already exists, ignore
                 const exists = found.subscribed.some((uid) => uid === member.id)
                 if (exists) {
                     continue
                 }
+                // add member to subscription list
                 found.subscribed.push(member.id)
             }
         }
 
         if (flag) {
-            // commit changes here
+            // commit changes
             const newBotData = JSON.stringify(newData)
             fs.writeFileSync('data/database.json', newBotData)
             data = newData
--- commands/subscription/unsubscribe.js
+++ commands/subscription/unsubscribe.js
@@ -8,38 +8,47 @@ module.exports = {
     usage: '<voiceChannels | all>',
     guildOnly: true,
     execute(message, args) {
+        // load in stored data
         const JSONData = fs.readFileSync('data/database.json')
         const newData = JSON.parse(JSONData)
         const guild = message.guild
         const member = message.member
+        // find matching server in data
         const server = newData.find((element) => element.serverID === guild.id)
+        // extract all voice channel in server
         const vcAll = guild.channels.cache.filter((channel) => channel.type === 'voice')
         let flag = true
 
+        // 'all' arg specified, unsub to all voice channels in the server
         if (args[0] === 'all' && args.length === 1) {
-            console.log('wow!')
             server.vc.forEach((channel) => {
                 const index = channel.subscribed.indexOf(member.id)
                 channel.subscribed.splice(index, 1)
             })
-        } else {
+        } 
+        // attempt to unsubscribe from all specified channel names
+        else {
             for (let i = 0; i < args.length; i++) {
+                // finding voice channel matching given name
                 const vc = vcAll.find((channel) => channel.name.toLowerCase() === args[i])
                 if (!vc) {
                     flag = false
                     break
                 }
+                // finding matching voice channel data entry
                 const found = server.vc.find((channel) => channel.vcID === vc.id)
                 if (!found) {
                     flag = false
                     break
                 }
+                // removing member from subscription list
                 const index = found.subscribed.indexOf(member.id)
                 found.subscribed.splice(index, 1)
             }
         }
 
         if (flag) {
+            // commit changes
             const newBotData = JSON.stringify(newData)
             fs.writeFileSync('data/database.json', newBotData)
             data = newData
