e90abb15f3657dd81aefef288988c1663bf8654b
kcai8672 <kcai8672@uni.sydney.edu.au>
2021-06-30 23:08:42 +1000

    Added voice channel activity event + server addition/deletion events

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 require('dotenv').config()
 const discord = require('discord.js')
+const { create } = require('domain')
 
 const client = new discord.Client()
+const prefix = process.env.PREFIX;
 
 const JSONData = fs.readFileSync('data/database.json')
 let data = JSON.parse(JSONData)
@@ -21,6 +23,14 @@ const createServer = (serverID) => {
     }
 }
 
+const createData = (serverID, data) => {
+    const guild = client.guilds.cache.get(serverID)
+    const obj = createServer(serverID)
+    const vc = guild.channels.cache.array().filter((voice) => voice.type === 'voice')
+    vc.forEach((channel) => obj.vc.push(createVC(channel.id)))
+    data.push(obj)
+}
+
 const updateDatabase = () => {
     const guilds = client.guilds.cache.array().map(server => server.id)
     // removing servers which the bot is not present in
@@ -28,13 +38,9 @@ const updateDatabase = () => {
     // adding servers which the bot has not registered
     for (let i = 0; i < guilds.length; i++) {
         const guildID = guilds[i]
-        const found = newData.some(server => server.serverID === guildID)
+        const found = newData.some((server) => server.serverID === guildID)
         if (!found) {
-            const newServer = createServer(guildID)
-            const guild = client.guilds.cache.get(guildID)
-            const vc = guild.channels.cache.array().filter(voice => voice.type === 'voice')
-            vc.forEach(channel => newServer.vc.push(createVC(channel.id)))
-            newData.push(newServer)
+            createData(guildID, newData)
         }
     }
     data = newData
@@ -55,26 +61,47 @@ client.on('message', (message) => {
 
 // event handler to monitor users joining voice channels
 client.on('voiceStateUpdate', (oldState, newState) => {
-    const guildID = oldState.guild.id
+    const guild = newState.guild
+    const guildID = newState.guild.id
     const oldChannel = oldState.channel
     const newChannel = newState.channel
     const member = newState.member
 
     // a new user has joined an empty voice channel
     if (!oldChannel && newChannel) {
-        console.log(`${member.displayName} joined ${newChannel.name}!`)
+        const channelID = newChannel.id
+        // console.log(member)
+        // console.log(`${member.displayName} joined ${newChannel.name}!`)
         const numUsers = newChannel.members.array().length
         if (numUsers === 1) {
-            const server = data.find((element) => {
-                element.s_id === guildID
+            const server = data.find((element) => element.serverID === guildID)
+            const subscriptions = server.vc.find((channel) => channel.vcID === channelID)
+            // console.log(subscriptions)
+            subscriptions.subscribed.forEach((user) => {
+                if (member.id != user) {
+                    const receiver = guild.members.cache.get(user)
+                    member.send(`${receiver}, ${member.displayName} joined the voice channel ${newChannel.displayName} in server \'${guild.name}\'!`)                        
+                }
             })
-            console.log(server.displayName)
         }
     }
 })
 
 // event handler for server joining
+client.on('guildCreate', (guild) => {
+    createData(guild.id, data)
+    console.log(data)
+    const newBotData = JSON.stringify(data)
+    fs.writeFileSync('data/database.json', newBotData)
+})
 
 //event handler for server leaving
+client.on('guildDelete', (guild) => {
+    const guildID = guild.id
+    const newData = data.filter((obj) => obj.serverID !== guildID)
+    data = newData
+    const newBotData = JSON.stringify(data)
+    fs.writeFileSync('data/database.json', newBotData)
+})
 
 client.login(process.env.TOKEN)
